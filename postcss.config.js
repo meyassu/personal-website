ac7086978a8c4cde39f21a6aec48887f6a269a82
Jadi Josh <meyassu@gmu.edu>
2023-09-01 16:56:26 -0400

    tailwind/postcss config

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+    plugins: [
+      require('tailwindcss'),  // Generates utility classes based on tailwind.config.js
+      require('autoprefixer'), // Adds vendor prefixes
+      require('cssnano')       // Minifies the CSS
+    ]
+  }
